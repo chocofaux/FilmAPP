ce906cd67177e756e03853532adc6331a1a1abba
Victorien <corbel.victorien@gmail.com>
2024-11-06 19:17:16 +0100

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="text-white p-4">
+      <h1>Bienvenue sur la page d'accueil</h1>
+      <p>Ceci est une page test pour vérifier le layout par défaut.</p>
+    </div>
+  </template>
+  
